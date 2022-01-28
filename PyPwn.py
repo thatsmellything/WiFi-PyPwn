6b657b5ec38015b8dc9fdcf2679d1f3c182f2b03
thatsmellything <thesmelliestman@gmail.com>
2022-01-28 11:32:29 -0700

    Now has 2 windows for access point discovery and device descovery

--- PyPwn.py
+++ PyPwn.py
@@ -1,12 +1,29 @@
-from guizero import App, Text, PushButton
+from guizero import App, Window, PushButton
 
-def change_message():
-    message.value = "You pressed the button!"
+def open_APScanner_window():
+    APScanner_window.show()
 
-app = App(title="Hello world")
+def close_APScanner_window():
+    APScanner_window.hide()
 
-message = Text(app, text="Welcome to the Hello world app!")
+def open_Scan4Devices_window():
+    Scan4Devices_window.show()
 
-button = PushButton(app, text="Press me", command=change_message)
+def close_Scan4Devices_window():
+    Scan4Devices_window.hide()
+
+app = App(title="PyPwn")
+
+APScanner_window = Window(app, title="Scan4AccessPoints")
+APScanner_window.hide()
+
+Scan4Devices_window = Window(app, title="Scan4Devices")
+Scan4Devices_window.hide()
+
+open_button = PushButton(app, text="Open AP Scanner", command=open_APScanner_window)
+close_button = PushButton(APScanner_window, text="Close", command=close_APScanner_window)
+
+Scan4Devices_button = PushButton(app, text="Open Device Scanner", command=open_Scan4Devices_window)
+Scan4Devices_button = PushButton(Scan4Devices_window, text="Close", command=close_Scan4Devices_window)
 
 app.display()
