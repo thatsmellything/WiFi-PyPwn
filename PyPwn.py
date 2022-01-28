e28c82f574a4d95bd819f1f8e6e614b5bf5b6ea3
thatsmellything <thesmelliestman@gmail.com>
2022-01-28 11:43:21 -0700

    Backend didn't work

--- PyPwn.py
+++ PyPwn.py
@@ -1,7 +1,30 @@
 from guizero import App, Window, PushButton
+import os
 
-import gui_backend
 
+
+#TEST ME
+os.system("echo Hello from the other side!")
+
+
+
+
+#GUI Backend
+def open_APScanner_window():
+    APScanner_window.show()
+
+def close_APScanner_window():
+    APScanner_window.hide()
+
+def open_Scan4Devices_window():
+    Scan4Devices_window.show()
+
+def close_Scan4Devices_window():
+    Scan4Devices_window.hide()
+
+
+
+#GUI Portion
 app = App(title="PyPwn")
 
 APScanner_window = Window(app, title="Scan4AccessPoints")
@@ -10,10 +33,10 @@ APScanner_window.hide()
 Scan4Devices_window = Window(app, title="Scan4Devices")
 Scan4Devices_window.hide()
 
-open_button = PushButton(app, text="Open AP Scanner", command=gui_backend.open_APScanner_window)
-close_button = PushButton(APScanner_window, text="Close", command=gui_backend.close_APScanner_window)
+open_button = PushButton(app, text="Open AP Scanner", command=open_APScanner_window)
+close_button = PushButton(APScanner_window, text="Close", command=close_APScanner_window)
 
-Scan4Devices_button = PushButton(app, text="Open Device Scanner", command=gui_backend.open_Scan4Devices_window)
-Scan4Devices_button = PushButton(Scan4Devices_window, text="Close", command=gui_backend.close_Scan4Devices_window)
+Scan4Devices_button = PushButton(app, text="Open Device Scanner", command=open_Scan4Devices_window)
+Scan4Devices_button = PushButton(Scan4Devices_window, text="Close", command=close_Scan4Devices_window)
 
 app.display()
