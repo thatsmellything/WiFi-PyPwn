a7b846f5acf3e9b618d33bff594d47c5ca2b7750
thatsmellything <thesmelliestman@gmail.com>
2022-01-28 11:17:15 -0700

    added button for testing

--- PyPwn.py
+++ PyPwn.py
@@ -1,5 +1,12 @@
-from guizero import App
+from guizero import App, Text, PushButton
+
+def change_message():
+    message.value = "You pressed the button!"
 
 app = App(title="Hello world")
+
+message = Text(app, text="Welcome to the Hello world app!")
+
+button = PushButton(app, text="Press me", command=change_message)
 
 app.display()
